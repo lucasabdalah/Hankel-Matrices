3d95e385514c96ae2e7e11149f99a1d07aa1f17d
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2021-05-11 07:38:11 -0300

    improve 1st image

--- matlab/part1/eigen_hankel.m
+++ matlab/part1/eigen_hankel.m
@@ -6,6 +6,9 @@
 %
 % 2021/04/29: - created by Vicente Zarzoso, UCA, CNRS, I3S, France.
 
+%
+close all; clearvars; clc;
+
 % simulation parameters
 num_dw = 5;                         % number of angular separation values
 num_N = 10;                         % number of sample size values
@@ -34,20 +37,44 @@ for i = 1:num_dw
     
 end
 
-% plot results
+% plot results - vz 
+% h = figure;
+% for i = 1:num_dw
+%     semilogy(N, cond_num(i, :)); hold on
+% end
+% grid; axis tight;
+% % set(gca, 'FontSize', 14)
+% xlabel('sample size, N');
+% ylabel('condition number');
+% legend(leg)
+
+%% Plot results - la
 h = figure;
-for i = 1:num_dw
-    semilogy(N, cond_num(i, :)); hold on
+myColor = {[0 0.4470 0.7410],[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560], [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330], [0.6350 0.0780 0.1840]};
+myLineStyle = {'--','-',':','-.'};
+myMarker = {'o', 'square', '^', 'x'};
+
+for ii = 1:num_dw
+    semilogy(N, cond_num(ii, :),...
+        'Color', myColor{ii},...        
+        'LineStyle', myLineStyle{rem(ii,4)+1},...
+        'LineWidth',1.0,...
+        'Marker',myMarker{rem(ii,4)+1},...
+        'MarkerFaceColor', myColor{ii},...
+        'MarkerSize', 3);
+    hold on;
 end
+hold off
+xlabel('N (Sample size)');
+ylabel('\sigma (Condition number)');
+legend(leg) % legend(leg,'Location', 'Best')
 grid; axis tight;
-% set(gca, 'FontSize', 14)
-xlabel('sample size, N');
-ylabel('condition number');
-legend(leg)
+ylim([0.5 1e8])
+
 
 %% conditionNumber - Export Result Figure
-% savefigPath = 'C:\Users\lukin\Documents\GitHub\Hankel-Matrices\matlab\fig';
-% outNamePDF = 'eigen_hankel-conditionNumber';
-% datetimestamp = datestr(now, 'yyyy-mm-dd');
-% outfilename = strjoin({[savefigPath '\' outNamePDF '-' datetimestamp]});
-% savefig_tight(h,outfilename);
+savefigPath = 'C:\Users\lukin\Documents\GitHub\Hankel-Matrices\matlab\fig';
+outNamePDF = 'eigen_hankel-conditionNumber2';
+datetimestamp = datestr(now, 'yyyy-mm-dd');
+outfilename = strjoin({[savefigPath '\' outNamePDF '-' datetimestamp]});
+savefig_tight(h,outfilename);
--- matlab/part3/noisy_proximity.m
+++ matlab/part3/noisy_proximity.m
@@ -1,4 +1,4 @@
-%% Computes the rank of an all-pole signal mapped into a Hankel matrix with various vector length.
+%% Computes the rank of an all-pole noisy signal mapped into a Hankel matrix with various vector length.
 %
 %
 %HISTORY:
@@ -25,7 +25,6 @@ wAll = logspace(-4,-1,spacings);
 %% Info storing
 hankelSample = zeros(length(itPoles),length(wAll)); % Store the number of samples to build a Hankel matrice with rank = number of poles
 LHsAll = zeros(length(itPoles),length(wAll));       % Store rank LHs Hankel
-fail2Compute = 1; % Store the number of fails to compute rank = Nexp
 
 %% Main algorithm
 for ii = 1:1:length(itPoles)
@@ -42,22 +41,22 @@ for ii = 1:1:length(itPoles)
         f = [linspace(0, wn*(Fn),Nexp)'];   % Linear Frequency
         comp = exp(1j*2*pi*f.*t);           % Poles
         s=sum(comp);  % Signal
+        
+        %% Noisy scenario with SNR = 310dB;
+        y = awgn(s,310,'measured');
 
         %% Study - Hankel
         HsRank = zeros(N,1);    % Store the rank for a signal with jj samples
-        Hs = Hankelize(s);      % Hankelize the whole signal
+        Hs = Hankelize(y);      % Hankelize the whole signal
         for kk=1:1:N % Computing Hankel matrix rank for a signal with jj samples 
-            HsRank(kk,1) = rank(Hankelize(s(1:kk)));  % Rank computing with s(1:jj)
+            HsRank(kk,1) = rank(Hankelize(y(1:kk)));  % Rank computing with s(1:jj)
         end
        
         %% Saving Hankel sample and current LHs rank
         if sum(find(HsRank == Nexp,1)) == 0 % Verify if the distance is enough to find the rank = number of poles
-            fail2Compute=fail2Compute+1;  
-            % fprintf(['we are in! => Fail no. ', num2str(fail2Compute),'\n']);
             hankelSample(ii,jj) = 150; % Replace when N isn't enough to find the rank = number of poles <======= here
         else
             hankelSample(ii,jj) = find(HsRank == Nexp,1);
-            % fprintf(['Rank =>', num2str(Nexp),'\t','Sample =>', num2str(hankelSample(ii,jj)),'\n']) 
         end
 
     end
@@ -67,8 +66,9 @@ for ii = 1:1:length(itPoles)
 
 end
 
-%% Plot results
+%% Plot results - SNR = 310
 h = figure;
+subplot(2,1,1)
 hold on
 myColor = {[0 0.4470 0.7410],[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560], [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330], [0.6350 0.0780 0.1840]};
 
@@ -82,23 +82,80 @@ for ii = 1:1:length(itPoles)
         'Marker',myMarker{rem(ii,4)+1},...
         'MarkerFaceColor', myColor{ii},...
         'MarkerSize', 3);
-    % if ii > 5
-    % line([min(wAll) max(wAll)], (2*itPoles(ii)-1)*[1 1],...
-    %     'Color', [0.1 0.1 0.1],...
-    %     'LineStyle','--',...
-    %     'HandleVisibility','off');
-    % end
 end
 hold off
 xlabel('\Delta\omega (\times \pi radians/samples)');
 ylabel('N (Sample size)');
 ylim([0 80])
-legend(leg) % legend(leg,'Location', 'Best')
+legend(leg,'Location', 'bestoutside') % legend(leg,'Location', 'Best')
 grid
 
-%% Hankel samples (rank = poles) - Export Result Figure
+%% Main algorithm 2
+for ii = 1:1:length(itPoles)
+    
+    Nexp = itPoles(ii); % Number of exponential
+    fprintf(['.............. Rank => ', num2str(Nexp),'..............\n']) 
+    
+    for jj = 1:1:length(wAll)
+        
+        %% Poles Setup
+        wn = wAll(jj);      % Normalized frequency distance  \Delta \omega
+        
+        %% Sum of Complex 
+        f = [linspace(0, wn*(Fn),Nexp)'];   % Linear Frequency
+        comp = exp(1j*2*pi*f.*t);           % Poles
+        s=sum(comp);  % Signal
+        
+        %% Noisy scenario with SNR = 50dB;
+        y = awgn(s,50,'measured');
+
+        %% Study - Hankel
+        HsRank = zeros(N,1);    % Store the rank for a signal with jj samples
+        Hs = Hankelize(y);      % Hankelize the whole signal
+        for kk=1:1:N % Computing Hankel matrix rank for a signal with jj samples 
+            HsRank(kk,1) = rank(Hankelize(y(1:kk)));  % Rank computing with s(1:jj)
+        end
+       
+        %% Saving Hankel sample and current LHs rank
+        if sum(find(HsRank == Nexp,1)) == 0 % Verify if the distance is enough to find the rank = number of poles
+            hankelSample(ii,jj) = 150; % Replace when N isn't enough to find the rank = number of poles <======= here
+        else
+            hankelSample(ii,jj) = find(HsRank == Nexp,1);
+        end
+
+    end
+
+    str = ['L = ', num2str(Nexp)]; % Save the legend
+    leg{ii} = str;
+
+end
+
+%% Plot results - SNR = 50
+subplot(2,1,2)
+hold on
+myColor = {[0 0.4470 0.7410],[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560], [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330], [0.6350 0.0780 0.1840]};
+
+myLineStyle = {'--','-',':','-.'};
+myMarker = {'o', 'square', '^', 'x'};
+for ii = 1:1:length(itPoles)
+    semilogx(wAll,hankelSample(ii,:),...
+        'Color', myColor{ii},...        
+        'LineStyle', myLineStyle{rem(ii,4)+1},...
+        'LineWidth',1.0,...
+        'Marker',myMarker{rem(ii,4)+1},...
+        'MarkerFaceColor', myColor{ii},...
+        'MarkerSize', 3);
+end
+hold off
+xlabel('\Delta\omega (\times \pi radians/samples)');
+ylabel('N (Sample size)');
+ylim([0 20])
+legend(leg,'Location', 'bestoutside') % legend(leg,'Location', 'Best')
+grid
+
+%% Hankel samples (rank = poles) in a noisy scenario - Export Result Figure
 savefigPath = 'C:\Users\lukin\Documents\GitHub\Hankel-Matrices\matlab\fig';
-outNamePDF = 'poles_proximity-PoleDistance_vs_Samples';
+outNamePDF = 'noisy_proximity-PoleDistance_vs_Samples';
 datetimestamp = datestr(now, 'yyyy-mm-dd');
 outfilename = strjoin({[savefigPath '\' outNamePDF '-' datetimestamp]});
 savefig_tight(h,outfilename);
